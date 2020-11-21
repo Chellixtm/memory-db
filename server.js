f5cbba8e42d6fadfcf8624e22f12d6d430aa9df5
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-11-21 14:51:17 -0700

    Updated server for testing with angular portion.

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const { getDecksByUser, getDeckById } = require('./query');
 
 app.set("port", (process.env.PORT || 5000));
 
-app.get("/getUserDeck", getDecksByUser);
+app.get("/deck/getUserDecks", getDecksByUser);
 
-app.get("/getDeck", getDeckById);
+app.get("/deck/getId", getDeckById);
 
 app.listen(app.get("port"), () => {
     console.log("Listening for connections on port: ", app.get("port"));
