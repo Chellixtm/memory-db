4d830cc8f5a18035c7ae46ccaeced6fe4f400c20
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-11-20 19:53:02 -0700

    Testing heroku postgress connection

--- query.js
+++ query.js
@@ -1,8 +1,30 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'qawpzrjqnhtiaf',
-    host: 'ec2-54-84-98-18.compute-1.amazonaws.com',
-    database: 'd988kso6rlku9m',
-    password: 'a23bf8141cd22e1349efcd30d2d7087cc2008acd3e21c34abe729a62a34c58cd',
-    port: 5432
-});
+const { Pool } = require('pg');
+const connString = process.env.DATABASE_URL || "postgres://qawpzrjqnhtiaf:a23bf8141cd22e1349efcd30d2d7087cc2008acd3e21c34abe729a62a34c58cd@ec2-54-84-98-18.compute-1.amazonaws.com:5432/d988kso6rlku9m";
+const pool = new Pool({connectionString: connString});
+
+exports.getTest =  function(req, res) {
+    getDecksFromDb((err, result) => {
+        if (err) {
+            res.status(500).json({success: false, data: err});
+        } else {
+            const decks = result[0];
+            res.status(200).json(decks);
+        }
+    });
+};
+
+function getDecksFromDb(callback) {
+    console.log("Getting decks from db.");
+    const sql = "SELECT * FROM flash_deck";
+    pool.query(sql, (err, res) => {
+        if (err) {
+            console.log("Error in query: ");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("Found decks.");
+
+        callback(null, res.rows);
+    });
+}
--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const db = require('./query');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+const { getTest } = require('./query');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/dist/'));
-app.use(cors());
-app.use(bodyparser.json());
-app.use(
-    bodyparser.urlencoded({extended:true})
-);
+app.set("port", (process.env.PORT || 5000));
 
+app.get("/getTest", getTest);
 
-
-
-app.get('/', (req, res) => {
-    res.json({info:'Node.js, Express, and Postgres API'});
+app.listen(app.get("port"), () => {
+    console.log("Listening for connections on port: ", app.get("port"));
 });
-app.listen(PORT, () => {
-    console.log('App running.');
-});
+
