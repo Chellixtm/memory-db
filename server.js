d5f7b3a2c6169f432075aa499d96a5b33619e282
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-12-15 18:33:40 -0700

    Removed session, caused crash

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const { getDecksByUser, getDeckById, postDeck, putDeck } = require('./db/deckQuery');
 
 
