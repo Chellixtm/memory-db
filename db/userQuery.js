ec03e0fae272b1395154ff09a8caa003b068de8a
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-12-15 18:36:46 -0700

    changed userInfo from const to let

--- db/userQuery.js
+++ db/userQuery.js
@@ -3,7 +3,7 @@ const dbCon = require('./dbConnection');
 const pool = dbCon.dbConnect(); 
 
 exports.signupUser = (req, res) => {
-    const userInfo;
+    let userInfo;
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         userInfo = [
